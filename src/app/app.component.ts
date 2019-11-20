3c43c4cc5e689510e4a2c428eb6851c203b439e4
pabloprog3 <pablo_h_n@yahoo.com.ar>
2019-11-20 05:00:30 -0300

    Configuracion de OneSignal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, AlertController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { OneSignal } from "@ionic-native/onesignal/ngx";
 
 @Component({
   selector: 'app-root',
@@ -13,7 +15,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private oneSignal:OneSignal,
+    private alertCtrl: AlertController
   ) {
     this.initializeApp();
   }
@@ -22,6 +26,28 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.setupNotificaciones();
     });
   }
+
+
+  setupNotificaciones(){
+      //this.oneSignal.setLogLevel({logLevel:5, visualLevel:5});
+      this.oneSignal.startInit('2901385f-7eb0-4a21-9f87-575b90f38dab', '72891117645');
+      this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
+      this.oneSignal.handleNotificationOpened().subscribe(valor=>{
+        this.presentAlert(valor);
+      });
+      this.oneSignal.endInit();
+  }
+
+  async presentAlert(valor) {
+    const alert = await this.alertCtrl.create({
+      header: valor.notification.payload.title,
+      message: valor.notification.payload.body,
+      buttons: ['Aceptar invitación', 'Rechazar']
+    });
+    await alert.present();
+  }
+
 }
